0ec2e8454d281d17558bd8b064e26ac0b8fc3ca3
Armando Marques <armando.jorge.marques@gmail.com>
2024-11-08 16:28:26 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import type { AppProps } from 'next/app';
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
@@ -7,12 +8,30 @@ import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+import { Authenticator } from '@aws-amplify/ui-react';
 
 Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
-export default function App() {
+
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+          <Component {...pageProps} />
+        </main>
+      )}
+    </Authenticator>
+  );
+};
+
+export  function App2() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
   function listTodos() {
